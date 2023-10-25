7d1419ab69210e2cb69ccce8b283e7770fe69fd4
HotDiddlyDog <kathyy.nguyen121@gmail.com>
2023-10-25 09:13:31 -0500

    navbar start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// import Home from "./pages/Home.js"
+// import Start from "./pages/Start.js"
+import { BrowserRouter as Router, Routes, Route }
+    from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
+    <Router basename={process.env.PUBLIC_URL}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Routes>
+        {/* <Route exact path='/' element={<Start />} />
+        <Route exact path='/home' element={<Home />} /> */}
+      </Routes>
     </div>
+    </Router>
   );
 }
 
