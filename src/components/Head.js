462bdbd14088b4fa73be79444045e20f8515df12
HotDiddlyDog <kathyy.nguyen121@gmail.com>
2023-10-28 15:24:25 -0500

    Cover text

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,7 +1,25 @@
 function Head() {
     return (
       <>
-      <img src={require("../assets/doctor.jpg")}></img>
+      <div class='container-header'>
+        <div class='gradient'>
+          <div class='text-container'>
+            <h1>Hidra Supra Awareness</h1>
+            <h2>What are the symptoms?</h2>
+            <p>sldkjflsdkjflskdjflskjdflksjdflksjdf
+              slkdjslkjdflksjdflkjdsflksjdlfkjsdlf
+              sdlkfjslkdjflskdjflksjdflksdjflksjdflksjd
+              slkdjflkajdlfkjasldkjflksadjflksjflkjsadf
+              lasdkjflakjsdlfkjasldkfjslakdjflaksjdflkasj
+              slkjfalksdjflkajsdlkfjsalkdjflksadjf
+              lksjdflkajsdlkfjslkadjflaksjdflkjsflksj
+              alksjdflkjsadlkfjalksjdflkajdslfkjsalkdf
+              lkasjdlkfjsalkdjflkajsdlfkajsdlkfjalksjf
+            </p>
+          </div>
+          <img class='image-head'src={require("../assets/cover.jpg")}></img>
+        </div>
+      </div>
       </>
     );
   }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,7 +7,7 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function Navigation() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-body-tertiary navbar" >
       <Container>
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
